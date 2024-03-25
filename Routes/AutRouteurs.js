133c7aa68c4104a69566d2e944244f6f88366a7e
Taharka2000 <sammbasow1999@gmail.com>
2024-03-25 01:58:32 +0000

    2emeCommit

--- Routes/AutRouteurs.js
+++ Routes/AutRouteurs.js
@@ -2,18 +2,25 @@ const express=require("express");
 const router=require("express").Router();
 const { signupUser, loginUser,createAccount, changePassword, userProfile}=require("../Controllers/AuthControllers");
 const { addFamille, deleteFamille, findFamille, updateFamille } = require("../Controllers/AddFamille");
-const { addMineur } = require("../Controllers/AddMineur");
+const { addMineur, findMineur, updateMineur, deleteMineur } = require("../Controllers/AddMineur");
 const { isAdmin, authenticateToken, isAuthenticated} = require("../Middlewares/VerifyRoje");
 const { authenticate} = require("../Middlewares/verifyTkoen");
 const { verifyTokenAndRole } = require("../Middlewares/authMiddleware");
+//pour authentification
 router.post("/signup",signupUser)
 router.post("/login",authenticate,loginUser)
 router.post("/auth/user",loginUser)
-router.post("/addFamille",verifyTokenAndRole,addFamille)
-router.post("/addMineur",isAuthenticated,isAdmin,addMineur)
 router.post("/admin",authenticate)
-router.post("/verification",createAccount)
+//ajout de famille
+router.post("/addFamille",verifyTokenAndRole,addFamille)
 router.get("/getFamille",findFamille)
 router.put("/updateFamille/:id",updateFamille)
 router.delete("/deleteFamille/:id",deleteFamille)
+//ajout de famille
+router.post("/addMineur",addMineur)
+router.get("/getMineur",findMineur)
+router.put("/updateMineur/:id",updateMineur)
+router.delete("/deleteMineur/:id",deleteMineur)
+//pour la creation du compte du agent
+router.post("/verification",createAccount)
 module.exports=router
